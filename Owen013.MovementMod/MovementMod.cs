2dda6d9c299dd46b231fc0dcc2675524c1fa7349
Owen013 <96493201+Owen013@users.noreply.github.com>
2021-12-24 14:32:50 -0500

    Add files via upload

--- Owen013.MovementMod/MovementMod.cs
+++ Owen013.MovementMod/MovementMod.cs
@@ -1,10 +1,14 @@
 ﻿using OWML.ModHelper;
 using OWML.Common;
+using UnityEngine.InputSystem;
 
 namespace MovementMod
 {
     public class MovementMod : ModBehaviour
     {
+        private PlayerCharacterController playerController;
+        private PlayerAnimController animController;
+
         private void Awake()
         {
             // You won't be able to access OWML's mod helper in Awake.
@@ -16,16 +20,47 @@ namespace MovementMod
         {
             // Starting here, you'll have access to OWML's mod helper.
             ModHelper.Console.WriteLine($"{nameof(MovementMod)} is loaded!", MessageType.Success);
-            
+
             // Example of accessing game code.
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
             {
                 if (loadScene != OWScene.SolarSystem && loadScene != OWScene.EyeOfTheUniverse) return;
-                var playerController = FindObjectOfType<PlayerCharacterController>();
+                playerController = FindObjectOfType<PlayerCharacterController>();
+                animController = FindObjectOfType<PlayerAnimController>();
                 playerController._useChargeJump = false;
                 playerController._strafeSpeed = 6f;
                 ModHelper.Console.WriteLine($"MovementMod loaded!", MessageType.Success);
             };
+        }
+        private void Update()
+        {
+            if (LoadManager.s_currentScene != OWScene.SolarSystem && LoadManager.s_currentScene != OWScene.EyeOfTheUniverse) return;
+            {
+                if (!OWInput.IsInputMode(InputMode.Menu))
+                {
+                    bool startSprint = false;
+                    bool stopSprint = false;
+                    if (Keyboard.current != null)
+                    {
+                        startSprint = Keyboard.current[Key.LeftAlt].wasPressedThisFrame;
+                        stopSprint = Keyboard.current[Key.LeftAlt].wasReleasedThisFrame;
+                        if (startSprint)
+                        {
+                            playerController._runSpeed = 9f;
+                            playerController._strafeSpeed = 9f;
+                            animController._animator.speed = 1.5f;
+                            ModHelper.Console.WriteLine($"Sprinting!", MessageType.Success);
+                        };
+                        if (stopSprint)
+                        {
+                            playerController._runSpeed = 6f;
+                            playerController._strafeSpeed = 6f;
+                            animController._animator.speed = 1f;
+                            ModHelper.Console.WriteLine($"Walking.", MessageType.Success);
+                        }
+                    }
+                }
+            }
         }
     }
 }
