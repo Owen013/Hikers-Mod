629041455518325d0f24b5cde62e9bc3ed6e1c07
Owen_013 <owen@crawfordsix.com>
2022-03-06 21:24:13 -0500

    Complete re-write, added climbing.

--- Owen013.MovementMod/HikersMod.cs
+++ Owen013.MovementMod/HikersMod.cs
@@ -6,187 +6,195 @@ namespace MovementMod
 {
     public class HikersMod : ModBehaviour
     {
-        // Setup() vars
-        private PlayerCharacterController playerController;
-        private PlayerAnimController animController;
-        public DreamLanternController dreamLantern;
+        // Config vars
+        public bool chargeJumpDisabled, slowStrafeDisabled, sprintEnabled, climbEnabled;
+        public float runSpeed, walkSpeed, jumpPower, sprintSpeed, climbPower, lastClimbTime, climbsPerJump, climbCooldownTime;
 
-        // Config options
-        public static bool disableChargeJump;
-        public static bool disableSlowerStrafing;
-        public static bool enableSprint;
-        public static float defaultSpeed;
-        public static float sprintSpeed;
-        public static float walkSpeed;
-        public static float jumpPower;
+        // Mod vars
+        public OWScene scene;
+        public PlayerCharacterController characterController;
+        public PlayerCameraController cameraController;
+        public PlayerAnimController animController;
+        public PlayerMovementAudio movementAudio;
+        public PlayerImpactAudio impactAudio;
+        public float runAnimSpeed, sprintAnimSpeed, walkAnimSpeed, strafeSpeed, sprintStrafeSpeed, climbsLeft;
 
-        // Sprinting patch vars
+        // Patch vars
         public static HikersMod Instance;
-        public bool IsDownThrustDisabled;
-        public bool IsFocusingLantern;
-        public bool StoppedFocusingLantern;
+        public bool disableDownThrust, focusingLantern, justUnfocusedLantern;
 
-        // Other vars
-        public static float strafeSpeed;
+        public override void Configure(IModConfig config)
+        {
+            base.Configure(config);
+            chargeJumpDisabled = config.GetSettingsValue<bool>("Disable Charge-Jump");
+            slowStrafeDisabled = config.GetSettingsValue<bool>("Disable Slow Strafing");
+            runSpeed = config.GetSettingsValue<float>("Default Run Speed");
+            walkSpeed = config.GetSettingsValue<float>("Walk Speed");
+            jumpPower = config.GetSettingsValue<float>("Jump Power");
+            sprintEnabled = config.GetSettingsValue<bool>("Enable Sprinting");
+            sprintSpeed = config.GetSettingsValue<float>("Sprint Speed");
+            climbEnabled = config.GetSettingsValue<bool>("Enable Climbing");
+            climbPower = config.GetSettingsValue<float>("Climb Jump Power");
+            climbsPerJump = config.GetSettingsValue<float>("Climb Jumps per Jump");
+            climbCooldownTime = config.GetSettingsValue<float>("Climb Jump Cooldown Time");
+            if (slowStrafeDisabled)
+            {
+                strafeSpeed = runSpeed;
+                sprintStrafeSpeed = sprintSpeed;
+            }
+            else
+            {
+                strafeSpeed = (2f / 3f) * runSpeed;
+                sprintStrafeSpeed = (2f / 3f) * sprintSpeed;
+            }
+            Setup();
+        }
 
         private void Awake()
         {
-            // Static reference to MovementMod so that it can be used in patches
+            // Static reference to HikersMod so it can be used in patches.
             Instance = this;
         }
 
         private void Start()
         {
-            ModHelper.Console.WriteLine($"{nameof(HikersMod)} is ready to go!", MessageType.Success);
-
-            // xen magic stuff
-            // We have to tell it to apply the patches we made below - xen-42
-            // Postfix means it calls the patch method after the original one is called - xen
-            ModHelper.HarmonyHelper.AddPostfix<JetpackThrusterController>
-                ("GetRawInput",
+            // Apply patches.
+            ModHelper.HarmonyHelper.AddPostfix<JetpackThrusterController>(
+                "GetRawInput",
                 typeof(Patches),
-                nameof(Patches.JetpackThrusterControllerGetRawInput));
-            ModHelper.HarmonyHelper.AddPostfix<PlayerCharacterController>
-                ("Awake",
+                nameof(Patches.GetJetpackInput));
+            ModHelper.HarmonyHelper.AddPostfix<PlayerCharacterController>(
+                "Awake",
                 typeof(Patches),
                 nameof(Patches.PlayerCharacterControllerAwake));
-            ModHelper.HarmonyHelper.AddPostfix<DreamLanternItem>
-                ("UpdateFocus",
+            ModHelper.HarmonyHelper.AddPostfix<DreamLanternItem>(
+                "UpdateFocus",
                 typeof(Patches),
                 nameof(Patches.DreamLanternFocusChanged));
-            ModHelper.Console.WriteLine($"Applied patches.");
-
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-            {
-                Setup();
-            };
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) => Setup();
+            ModHelper.Console.WriteLine($"{nameof(HikersMod)} is ready to go!", MessageType.Success);
         }
 
-        // This runs whenever config.json is changed.
-        public override void Configure(IModConfig config)
-        {
-            // Update config vars
-            base.Configure(config); // Don't know what this does, but Configure() automatically creates it.
-            disableChargeJump = config.GetSettingsValue<bool>("Disable Charge-Jump");
-            disableSlowerStrafing = config.GetSettingsValue<bool>("Disable Slower Strafing");
-            enableSprint = config.GetSettingsValue<bool>("Enable Sprinting");
-            defaultSpeed = config.GetSettingsValue<float>("Default Speed");
-            sprintSpeed = config.GetSettingsValue<float>("Sprint Speed");
-            walkSpeed = config.GetSettingsValue<float>("Walk Speed");
-            jumpPower = config.GetSettingsValue<float>("Jump Power");
-
-            // Update strafe speed
-            if (disableSlowerStrafing) strafeSpeed = defaultSpeed;
-            else strafeSpeed = defaultSpeed * 2 / 3;
-            Setup();
-        }
-
-        // This is where MovementMod features are enabled or disabled.
-        public void Setup()
-        {
-            if (LoadManager.s_currentScene != OWScene.SolarSystem
-                && LoadManager.s_currentScene != OWScene.EyeOfTheUniverse
-                && playerController == null)
-                return;
-            playerController = FindObjectOfType<PlayerCharacterController>();
-            ModHelper.Events.Unity.FireInNUpdates(() => animController = FindObjectOfType<PlayerAnimController>(), 60);
-            playerController._useChargeJump = !disableChargeJump;
-            playerController._runSpeed = defaultSpeed;
-            playerController._strafeSpeed = strafeSpeed;
-            playerController._walkSpeed = walkSpeed;
-            playerController._maxJumpSpeed = jumpPower;
-            ModHelper.Events.Unity.FireInNUpdates(() =>
-                animController._animator.speed = Mathf.Max(defaultSpeed / 6, 1), 60);
-        }
-
-        // Every frame
         private void Update()
         {
-            if (LoadManager.s_currentScene != OWScene.SolarSystem
-                && LoadManager.s_currentScene != OWScene.EyeOfTheUniverse)
-                return;
+            if (scene == OWScene.SolarSystem || scene == OWScene.EyeOfTheUniverse)
             {
-                if (!OWInput.IsInputMode(InputMode.Menu))
+                var state = Locator.GetPlayerController();
+                // Sprinting
+                if (sprintEnabled)
                 {
-                    bool startSprint;
-                    bool stopSprint;
-                    bool startWalking;
-                    bool stopWalking;
-                    // We use the same controls as the downward thrust input for the jetpack - xen
-                    startSprint = OWInput.IsNewlyPressed(InputLibrary.thrustDown);
-                    stopSprint = OWInput.IsNewlyReleased(InputLibrary.thrustDown);
-                    startWalking = OWInput.IsPressed(InputLibrary.rollMode);
-                    stopWalking = OWInput.IsNewlyReleased(InputLibrary.rollMode);
-                    // To sprint we have to be standing on the ground - xen
-                    if ((startSprint && Locator.GetPlayerController().IsGrounded() && !OWInput.IsPressed(InputLibrary.rollMode))
-                        || (stopWalking || StoppedFocusingLantern)
-                            && OWInput.IsPressed(InputLibrary.thrustDown)
-                            && Locator.GetPlayerController().IsGrounded())
-                        StartSprinting();
-                    if (stopSprint || startWalking)
-                        StopSprinting();
-                    if (startWalking)
-                        animController._animator.speed = Mathf.Max(walkSpeed / 6, 1);
-                    if (IsFocusingLantern)
-                        animController._animator.speed = 1f;
+                    bool canSprint = state._isGrounded && !OWInput.IsPressed(InputLibrary.rollMode);
+                    bool sprintPressed = OWInput.IsPressed(InputLibrary.thrustDown);
+                    bool startSprint = OWInput.IsNewlyPressed(InputLibrary.thrustDown);
+                    bool stopSprint = OWInput.IsNewlyReleased(InputLibrary.thrustDown);
+                    bool startWalk = OWInput.IsPressed(InputLibrary.rollMode);
+                    bool stopWalk = OWInput.IsNewlyReleased(InputLibrary.rollMode);
+                    if (canSprint && (startSprint || ((stopWalk || justUnfocusedLantern) && sprintPressed))) Sprint(true);
+                    if (stopSprint || startWalk || focusingLantern) Sprint(false);
+                    justUnfocusedLantern = false;
+                }
+                // Wall Jumping
+                if (climbEnabled)
+                {
+                    characterController.UpdatePushable();
+                    bool wallJumping = OWInput.IsNewlyPressed(InputLibrary.jump);
+                    bool isGrounded = state.IsGrounded();
+                    bool isZeroG = state._isZeroGMovementEnabled;
+                    bool jumpCooldown = Time.time - characterController._lastJumpTime < climbCooldownTime;
+                    bool wallJumpCooldown = Time.time - lastClimbTime < climbCooldownTime;
+                    bool canWallJump = state._isPushable
+                                    && climbsLeft > 0
+                                    && !isGrounded
+                                    && !isZeroG
+                                    && !jumpCooldown
+                                    && !wallJumpCooldown;
+                    if (wallJumping && canWallJump) Climb();
                 }
             }
         }
 
-        // Sprinting functions
-        public void StartSprinting()
+        private void Setup()
         {
-            if (enableSprint != true) return;
-            IsDownThrustDisabled = true;
-            playerController._runSpeed = sprintSpeed;
-            playerController._strafeSpeed = sprintSpeed / defaultSpeed * strafeSpeed;
-            animController._animator.speed = Mathf.Max(sprintSpeed / 6, 1);
+            scene = LoadManager.s_currentScene;
+            if (scene == OWScene.SolarSystem || scene == OWScene.EyeOfTheUniverse)
+            {
+                characterController = FindObjectOfType<PlayerCharacterController>();
+                cameraController = FindObjectOfType<PlayerCameraController>();
+                animController = FindObjectOfType<PlayerAnimController>();
+                movementAudio = FindObjectOfType<PlayerMovementAudio>();
+                impactAudio = FindObjectOfType<PlayerImpactAudio>();
+                characterController._useChargeJump = !chargeJumpDisabled;
+                characterController._runSpeed = runSpeed;
+                characterController._strafeSpeed = strafeSpeed;
+                characterController._walkSpeed = walkSpeed;
+                characterController._maxJumpSpeed = jumpPower;
+                runAnimSpeed = Mathf.Max(runSpeed / 6, 1);
+                sprintAnimSpeed = Mathf.Max(sprintSpeed / 6, 1);
+                walkAnimSpeed = Mathf.Max(walkSpeed / 6, 1);
+                if (characterController._isGrounded || climbsLeft > climbsPerJump)
+                    climbsLeft = climbsPerJump;
+            };
         }
 
-        public void StopSprinting()
+        public void Sprint(bool sprinting)
         {
-            if (enableSprint != true) return;
-            IsDownThrustDisabled = false;
-            playerController._runSpeed = defaultSpeed;
-            playerController._strafeSpeed = strafeSpeed;
-            animController._animator.speed = Mathf.Max(defaultSpeed / 6, 1);
+            if (sprinting)
+            {
+                characterController._runSpeed = sprintSpeed;
+                characterController._strafeSpeed = sprintStrafeSpeed;
+                animController._animator.speed = sprintAnimSpeed;
+                disableDownThrust = true;
+            }
+            else
+            {
+                characterController._runSpeed = runSpeed;
+                characterController._strafeSpeed = strafeSpeed;
+                animController._animator.speed = runAnimSpeed;
+                disableDownThrust = false;
+            }
+        }
+
+        public void Climb()
+        {
+            float thisClimbPower = (climbPower * 0.75f) * (climbsLeft / climbsPerJump) + climbPower * 0.25f;
+            Vector3 climbVelocity = new Vector3(0, thisClimbPower, 0);
+            climbsLeft -= 1;
+            lastClimbTime = Time.time;
+            characterController._owRigidbody.SetVelocity(
+                characterController._pushableBody.GetPointVelocity(characterController._pushContactPt));
+            characterController._owRigidbody.AddLocalVelocityChange(climbVelocity);
+            animController.OnPlayerJump();
+            movementAudio.OnJump();
         }
     }
 
     public static class Patches
     {
-        // To disable the jetpack downward thrust while on the ground we have to patch the GetRawInput method in the JetpackThrusterController class - xen
-        public static void JetpackThrusterControllerGetRawInput(ref Vector3 __result)
+        public static void GetJetpackInput(ref Vector3 __result)
         {
-            // When grounded, don't let the input have any downward component in the y-direction - xen
-            if (HikersMod.Instance.IsDownThrustDisabled)
-            {
-                if (__result.y < 0) __result.y = 0;
-            }
+            if (HikersMod.Instance.disableDownThrust && __result.y < 0) __result.y = 0;
         }
 
-        // When the player character controller is loaded we add to one of its events - xen
         public static void PlayerCharacterControllerAwake(PlayerCharacterController __instance)
         {
-            // If we are flying and just became grounded and control is still held then we have to start running - xen
             __instance.OnBecomeGrounded += () =>
             {
+                HikersMod.Instance.climbsLeft = HikersMod.Instance.climbsPerJump;
                 if (OWInput.IsPressed(InputLibrary.thrustDown) && !OWInput.IsPressed(InputLibrary.rollMode))
-                {
-                    // This is where the Instance variable we made comes in use - xen
-                    HikersMod.Instance.StartSprinting();
-                }
+                    HikersMod.Instance.Sprint(true);
             };
         }
-        
+
         public static void DreamLanternFocusChanged(DreamLanternItem __instance)
         {
-            HikersMod.Instance.StoppedFocusingLantern = true;
-            if (__instance._focusing == true)
-                HikersMod.Instance.IsFocusingLantern = true;
-            else if (HikersMod.Instance.IsFocusingLantern == true)
-                HikersMod.Instance.IsFocusingLantern = false;
-                HikersMod.Instance.StoppedFocusingLantern = true;
+            bool lanternIsFocused = __instance._focusing;
+            // If the lantern was just unfocused...
+            if (!lanternIsFocused && HikersMod.Instance.focusingLantern)
+            {
+                HikersMod.Instance.focusingLantern = false;
+                HikersMod.Instance.justUnfocusedLantern = true;
+            }
+            else if (lanternIsFocused) HikersMod.Instance.focusingLantern = true;
         }
     }
 }
