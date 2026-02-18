35d1ccd1f7bab548585bde346dfea98b307d6fce
owen <owen@OWENS_DESKTOP>
2026-02-17 23:49:27 -0500

    changed AnimSpeedController ground speed calculation, removed check for Immersion anim speed (Immersion doesn't change anim speed anymore)

--- Owen013.MovementMod/Components/AnimSpeedController.cs
+++ Owen013.MovementMod/Components/AnimSpeedController.cs
@@ -16,11 +16,19 @@ public class AnimSpeedController : MonoBehaviour
 
     private void LateUpdate()
     {
-        float groundSpeed = _characterController.GetRelativeGroundVelocity().magnitude * (ModMain.SmolHatchlingAPI?.GetPlayerAnimSpeed() ?? 1);
+        // change viewbob strength quickly if on ground
+        Vector3 groundVel = _characterController.GetRelativeGroundVelocity();
+        groundVel.y = 0f;
+        if (Mathf.Abs(groundVel.x) < 0.05f)
+            groundVel.x = 0f;
+        if (Mathf.Abs(groundVel.z) < 0.05f)
+            groundVel.z = 0f;
+        float groundSpeed = groundVel.magnitude;
+        if (ModMain.SmolHatchlingAPI != null)
+            groundSpeed *= ModMain.SmolHatchlingAPI.GetPlayerAnimSpeed();
         float animSpeedMultiplier = Mathf.Sqrt(groundSpeed / 6);
         float floatyPhysicsMultiplier = Mathf.Sqrt(_characterController._acceleration / Config.GroundAccel);
-        float underwaterMultiplier = ModMain.ImmersionAPI != null ? ModMain.ImmersionAPI.GetAnimSpeed() : 1;
 
-        _animator.speed = _characterController.IsGrounded() ? Mathf.Max(animSpeedMultiplier * floatyPhysicsMultiplier, floatyPhysicsMultiplier) : underwaterMultiplier;
+        _animator.speed = Mathf.Max(animSpeedMultiplier * floatyPhysicsMultiplier, floatyPhysicsMultiplier);
     }
 }
